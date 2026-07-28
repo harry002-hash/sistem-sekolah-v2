267b8d774ab97159fb6d090a1d9314e9c436e7d2
harryvanessa <harvy.zeus36@gmail.com>
2026-07-28 14:25:47 +0700

    Fixing

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::name('students.')->prefix('students')->group(function () {
 
     Route::get('/{id}', [StudentController::class, 'show'])->name('show');
 
-    Route::get('/create', [StudentController::class, 'create'])->name('create');
+    Route::get('/create', [StudentController::class, 'create'])->name('create')->whereNumber('id');
 
     Route::get('/{id}/edit', [StudentController::class, 'edit'])->name('edit');
 
@@ -37,47 +37,47 @@ Route::name('students.')->prefix('students')->group(function () {
 
 // Menejemen Data Guru (Action Controller)
 
-Route::name('teacher.')->prefix('teacher')->group(function() {
- 
-Route::get('/', [TeacherController::class, 'index'])->name('index');
- 
-Route::get('/{id}', [TeacherController::class, 'show'])->name('show');
- 
-Route::get('/create', [TeacherController::class, 'create'])->name('create');
- 
-Route::get('/{id}/edit', [TeacherController::class, 'edit'])->name('edit');
- 
-Route::post('/', [TeacherController::class, 'store'])->name('store');
- 
-Route::put('/{id}', [TeacherController::class, 'update'])->name('update');
- 
-Route::delete('{id}', [TeacherController::class, 'destroy'])->name('destroy');
- 
+Route::name('teachers.')->prefix('teachers')->group(function () {
+
+    Route::get('/', [TeacherController::class, 'index'])->name('index');
+
+    Route::get('/{id}', [TeacherController::class, 'show'])->name('show')->whereNumber('id');
+    
+    Route::get('/create', [TeacherController::class, 'create'])->name('create');
+
+    Route::get('/{id}/edit', [TeacherController::class, 'edit'])->name('edit');
+
+    Route::post('/', [TeacherController::class, 'store'])->name('store');
+
+    Route::put('/{id}', [TeacherController::class, 'update'])->name('update');
+
+    Route::delete('{id}', [TeacherController::class, 'destroy'])->name('destroy');
+
 });
 
 // Manajemen Data Kelas Sekolah (Invokable)
 
-Route::name('classes.')->prefix('classes')->group(function() {
- 
-Route::get('/', IndexController::class)->name('index');
- 
-Route::get('/{id}', ShowController::class)->name('show');
- 
-Route::get('/create', CreateController::class)->name('create');
- 
-Route::get('/{id}/edit', EditController::class)->name('edit');
- 
-Route::post('/', StoreController::class)->name('store');
- 
-Route::put('/{id}', UpdateController::class)->name('update');
- 
-Route::delete('/{id}', DestroyController::class)->name('destroy');
- 
+Route::name('classes.')->prefix('classes')->group(function () {
+
+    Route::get('/', IndexController::class)->name('index');
+
+    Route::get('/{id}', ShowController::class)->name('show')->whereNumber('id');
+
+    Route::get('/create', CreateController::class)->name('create   ');
+
+    Route::get('/{id}/edit', EditController::class)->name('edit');
+
+    Route::post('/', StoreController::class)->name('store');
+
+    Route::put('/{id}', UpdateController::class)->name('update');
+
+    Route::delete('/{id}', DestroyController::class)->name('destroy');
+
 });
 
 // Manajemen Data Jurusan (Resource Controller)
 Route::resource('majors', MajorController::class);
-    
+
 
 
 
